10d47abb6d3797b54e5c957bd2ba859d348f6608
Kasyap-R <kasyap.rachuri2@gmail.com>
2024-07-14 01:20:28 -0400

    Modified docker compose

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,12 @@
-import React from 'react';
-import './App.css';
-import {Page} from './stores/PageStore.ts';
-import usePageStore from './stores/PageStore.ts';
-import Home from './pages/Home.tsx';
+import React from "react";
+import "./App.css";
+import { Page } from "./stores/PageStore.ts";
+import usePageStore from "./stores/PageStore.ts";
+import Home from "./pages/Home.tsx";
 
 function App() {
-  const {currentPage} = usePageStore();
   return (
-    <main>
-      {currentPage == Page.HOME && <Home/>}
-    </main>
+    <main>{usePageStore.getState().currentPage == Page.HOME && <Home />}</main>
   );
 }
 
